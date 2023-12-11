f7bdd902eb0565cb5edfb0b83cb19f26c02c2bf9
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-12-11 15:47:43 +0530

    Added: JUnit Test for all the functionalities present

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -12,4 +12,54 @@ public class UserRegistration {
 
         return isValid;
     }
+    //    @Desc:- it is a class which checks last name and validate it
+//    @Param:- takes last name as the input to check
+//    @Return:- return true/false on the basis of input
+    public boolean ValidateLastName(String LastName){
+        boolean isValid = LastName.matches("[A-Z][a-z]{3,15}");
+        if(!isValid){
+            System.out.println("last name is not valid");
+        }
+
+        return isValid;
+    }
+
+    //    @Desc:- it is a class which checks email and validate it
+//    @Param:- takes email as the input to check
+//    @Return:- return true/false on the basis of input
+    public boolean ValidateEmailId(String Email) {
+        boolean isValidEmail = Email.matches("^[A-Za-z][A-Za-z0-9.-_%+-]+@[A-Za-z0-9-_%+-]+(\\.[A-Za-z]{2,}){1,2}$");
+        if (!isValidEmail) {
+            System.out.println("Email id is not valid ");
+        }
+        return isValidEmail;
+    }
+
+    //    @Desc:- it is a class which checks mobile number and validate it
+//    @Param:- takes number as the input to check
+//    @Return:- return true/false on the basis of input
+    public boolean ValidateMobileNumber(String Number) {
+        boolean isValidMobileNumber = Number.matches("^\\+\\d{2}?\\d{10}$");
+        if (!isValidMobileNumber)
+        {
+            System.out.println("mobile number is not valid ");
+        }
+        return isValidMobileNumber;
+    }
+
+    //    @Desc:- it is a class which checks Password and validate it
+//    @Param:- takes password as the input to check
+//    @Return:- return true/false on the basis of input
+    public boolean ValidatePassword(String password) {
+//        we are applying different conditions to check different things in password like
+//        ( for AT LEAST ONE UPPERCASE WE USED (?=.*[A-Z]))
+//        (FOR MINIMUM LENGTH 8 WE USED {8,})
+//        (FOR ATLEAST ONE NUMERIC DIGIT (?=.*\d))
+
+        boolean isValidPassword = password.matches("^(?=.*[A-Z])(?=.*\\d)(?=.*[.@$!%*?&])[A-Za-z0-9.@$!%*?&]{8,}$");
+        if (!isValidPassword) {
+            System.out.println("Password invalid");
+        }
+        return isValidPassword;
+    }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,5 +1,55 @@
 package com.bridgelabz;
-
+import org.junit.Assert;
+import org.junit.Test;
 public class UserRegistrationTest {
     UserRegistration userRegistration = new UserRegistration();
+    Assert Assertions = null;
+
+    /*ValidFirstName method is testing a First name*/
+    @Test
+    public void ValidFirstName(){
+        boolean a = userRegistration.ValidateFirstName("Sanskar");
+        boolean b = userRegistration.ValidateFirstName("sanskar");
+        Assertions.assertTrue(a);
+        Assertions.assertFalse(b);
+    }
+
+    /*ValidLastName method is testing a last name*/
+    @Test
+    public void ValidLastName()
+    {
+        boolean a =userRegistration.ValidateLastName("Soni");
+        boolean b=userRegistration.ValidateLastName("soni");
+        Assertions.assertTrue(a);
+        Assertions.assertFalse(b);
+    }
+    /*ValidEmailId method is testing an email id*/
+    @Test
+    public void ValidEmailId()
+    {
+        boolean a=userRegistration.ValidateEmailId("Sanskarsoni89@gmail.com");
+        boolean b=userRegistration.ValidateEmailId("sanskar.gmail.com");
+        Assertions.assertTrue(a);
+        Assertions.assertFalse(b);
+    }
+
+    /*ValidMobileNo method is testing a mobile number*/
+    @Test
+    public void ValidMobileNo()
+    {
+        boolean a=userRegistration.ValidateMobileNumber("+919887279510");
+        boolean b=userRegistration.ValidateMobileNumber("755879757");
+        Assertions.assertTrue(a);
+        Assertions.assertFalse(b);
+    }
+
+    /*ValidPassword method is testing a password */
+    @Test
+    public void ValidPassword()
+    {
+        boolean a=userRegistration.ValidatePassword("Sanskar@123");
+        boolean b=userRegistration.ValidatePassword("sanskar");
+        Assertions.assertTrue(a);
+        Assertions.assertFalse(b);
+    }
 }
